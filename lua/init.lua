b3cdc86429334a40c3ed218c232fbef7c9dac210
Vladimir Gnidenko <vladimir.hnidenko@outlook.com>
2024-01-24 23:31:27 +0100

    refactored to eliminate non vscode plugs

--- lua/init.lua
+++ lua/init.lua
@@ -21,8 +21,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-plugins = { 
-	{ 
+plugins = {
+
+	{
 	  "nvim-tree/nvim-tree.lua", version = "*", lazy = false,
 	  dependencies = { "nvim-tree/nvim-web-devicons", },
 	  config = function()
@@ -42,7 +43,7 @@ plugins = {
 		}
 	  end,
 	},
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
+
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
@@ -89,6 +90,7 @@ plugins = {
 			}
 		end
 	},
+
 	{
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
@@ -99,6 +101,7 @@ plugins = {
 			})
 		end
 	},
+
 	{ 'smoka7/hop.nvim', version = "*", opts = {}, },
 
 	{ 'wellle/targets.vim' },
@@ -107,8 +110,82 @@ plugins = {
 	{ 'kana/vim-operator-user' },
 }
 
+
 if not vim.g.vscode then
-	table.insert(plugins, { "nvim-treesitter/nvim-treesitter-context" })
+
+	table.insert(plugins, {
+		"nvim-tree/nvim-tree.lua", version = "*", lazy = false,
+		dependencies = { "nvim-tree/nvim-web-devicons", },
+		config = function()
+		  require("nvim-tree").setup {
+			sort = {
+			  sorter = "case_sensitive",
+			},
+			view = {
+			  width = 30,
+			},
+			renderer = {
+			  group_empty = true,
+			},
+			filters = {
+			  dotfiles = true,
+			},
+		  }
+		end,
+	  })
+  
+	  table.insert(plugins, {
+		  'nvim-lualine/lualine.nvim',
+		  dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+		  config = function()
+			  require('lualine').setup {
+				  options = {
+					  icons_enabled = true,
+					  theme = 'auto',
+					  component_separators = { left = '', right = ''},
+					  section_separators = { left = '', right = ''},
+					  disabled_filetypes = {
+						  statusline = {},
+						  winbar = {},
+					  },
+					  ignore_focus = {},
+					  always_divide_middle = true,
+					  globalstatus = false,
+					  refresh = {
+						  statusline = 1000,
+						  tabline = 1000,
+						  winbar = 1000,
+					  }
+				  },
+				  sections = {
+					  lualine_a = {'mode'},
+					  lualine_b = {'branch', 'diff', 'diagnostics'},
+					  lualine_c = {'filename'},
+					  lualine_x = {'encoding', 'fileformat', 'filetype'},
+					  lualine_y = {'progress'},
+					  lualine_z = {'location'}
+				  },
+				  inactive_sections = {
+					  lualine_a = {},
+					  lualine_b = {},
+					  lualine_c = {'filename'},
+					  lualine_x = {'location'},
+					  lualine_y = {},
+					  lualine_z = {}
+				  },
+				  tabline = {},
+				  winbar = {},
+				  inactive_winbar = {},
+				  extensions = {}
+			  }
+		  end
+	  })
+	  
+	table.insert(plugins, {
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" }
+	})
+
 	table.insert(plugins, { 'RRethy/vim-illuminate', config = function()
 		require'illuminate'.configure {
 			providers = { 'lsp', 'treesitter', 'regex' },
@@ -116,12 +193,26 @@ if not vim.g.vscode then
 			under_cursor = true,
 		}
 	end })
-	table.insert(plugins, { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } })
+
+	table.insert(plugins, {
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<C-p>'     , builtin.find_files, {})
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		end
+	})
+
 	table.insert(plugins, { "neovim/nvim-lspconfig" })
 	table.insert(plugins, { "nvim-lua/lsp-status.nvim" })
 	table.insert(plugins, { "nvim-lua/lsp-status.nvim" })
-	table.insert(plugins, { "nvim-treesitter/nvim-treesitter-context" })
 end
+
 
 -- lazy PM load
 require("lazy").setup(plugins, {
@@ -131,15 +222,6 @@ require("lazy").setup(plugins, {
 	}
 })
 
--- Telestope configuration
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>'     , builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- Lualine configuration
 -- Hop configuration
 require("hop").setup { }
 vim.keymap.set('n', '<leader><leader>w', require'hop'.hint_words, {})
