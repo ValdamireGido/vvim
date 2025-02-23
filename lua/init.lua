db42417ca2058a7d8183baa26a2277bb2e17c185
Vladimir Gnidenko <vladimir.hnidenko@outlook.com>
2025-02-23 18:23:01 +0100

    Update telescope layout

--- lua/init.lua
+++ lua/init.lua
@@ -196,16 +196,41 @@ if not vim.g.vscode then
 
 	table.insert(plugins, {
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
+		tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
+			require('telescope').setup({
+				defaults = {
+					layout_strategy = "center",
+					layout_config = {
+						vertical = {
+							mirror = true,
+						},
+						center = {
+							mirror = true,
+						}
+					},
+					sorting_strategy = "ascending",
+					path_display = {
+						shorten = true,
+						truncate = true,
+					},
+				},
+			})
 			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<C-p>'     , builtin.find_files, {})
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 		end
+	})
+	table.insert(plugins, {
+		'nvim-telescope/telescope-frecency.nvim',
+		version = "*",
+		config = function()
+			require("telescope").load_extension("frecency")
+			vim.keymap.set('n', '<C-p>', ':Telescope frecency workspace=CWD<CR>')
+		end,
 	})
 
 	table.insert(plugins, { "neovim/nvim-lspconfig" })
