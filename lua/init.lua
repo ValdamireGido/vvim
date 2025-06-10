68f6135818746ab4fe3023423bdead2a56a60f81
Vladimir Gnidenko <valdamire@gmail.com>
2025-06-10 18:10:56 +0200

    Changed file name to be relative path in lualine

--- lua/init.lua
+++ lua/init.lua
@@ -23,53 +23,6 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
 
 	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-		config = function()
-			require('lualine').setup {
-				options = {
-					icons_enabled = true,
-					theme = 'auto',
-					component_separators = { left = '', right = '' },
-					section_separators = { left = '', right = '' },
-					disabled_filetypes = {
-						statusline = {},
-						winbar = {},
-					},
-					ignore_focus = {},
-					always_divide_middle = true,
-					globalstatus = false,
-					refresh = {
-						statusline = 1000,
-						tabline = 1000,
-						winbar = 1000,
-					}
-				},
-				sections = {
-					lualine_a = { 'mode' },
-					lualine_b = { 'branch', 'diff', 'diagnostics' },
-					lualine_c = { 'filename' },
-					lualine_x = { 'encoding', 'fileformat', 'filetype' },
-					lualine_y = { 'progress' },
-					lualine_z = { 'location' }
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { 'filename' },
-					lualine_x = { 'location' },
-					lualine_y = {},
-					lualine_z = {}
-				},
-				tabline = {},
-				winbar = {},
-				inactive_winbar = {},
-				extensions = {}
-			}
-		end
-	},
-
-	{
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
@@ -335,7 +288,7 @@ if not vim.g.vscode then
 				sections = {
 					lualine_a = { 'mode' },
 					lualine_b = { 'branch', 'diff', 'diagnostics' },
-					lualine_c = { 'filename' },
+					lualine_c = { { 'filename', file_status = true, path = 1 } },
 					lualine_x = { 'encoding', 'fileformat', 'filetype' },
 					lualine_y = { 'progress' },
 					lualine_z = { 'location' }
@@ -343,7 +296,7 @@ if not vim.g.vscode then
 				inactive_sections = {
 					lualine_a = {},
 					lualine_b = {},
-					lualine_c = { 'filename' },
+					lualine_c = { { 'filename', file_status = true, path = 1 } },
 					lualine_x = { 'location' },
 					lualine_y = {},
 					lualine_z = {}
